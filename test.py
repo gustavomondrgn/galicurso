6a7aac8726f1563990c127a52fa10da3ffbe0fce
Gustavo <gustavo@kandle.studio>
2026-02-16 05:02:51 -0300

    Quarto Commit, agora tendo GitHub

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ e = 15
 
 meu_nome = "Gustavo"
 minha_idade = 25
+riqueza = 40
 
-print(f"Meu nome é, {meu_nome} e eu tenho {minha_idade} anos de idade")
+print(f"Meu nome é, {meu_nome} e eu tenho {minha_idade} anos de idade e eu tenho {riqueza} milhões em patrimônio líquido")
