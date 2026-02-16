da1bade58cbedc25ba7d80842cfe61eb270a3351
Gustavo <gustavo@kandle.studio>
2026-02-16 05:25:56 -0300

    Aula 04 - Tipos de Dados

---
+++ Aulas/aula04-tipos-de-dados.py
@@ -0,0 +1,34 @@
+# Aula sobre tipos de dados
+# Basicamente, nós temos 4 tipos de dados:
+# Inteiros, Floats, Strings e Booleanos 
+# Testando abaixo:
+
+# Definindo alguns dados
+
+dado_inteiro = 10
+dado_inteiro2 = 20
+dado_inteiro3 = 10
+
+dado_float = 3.14
+
+dado_string = "Gustavo codando pra carai"
+dado_string_alfanumero = "Gustavo codando pra carai em 2026 é real oficial"
+
+dado_booleanum = True
+
+
+# Verificando os tipos de dados das variáveis que criei
+
+print(type(dado_inteiro))
+print(type(dado_float))
+print(type(dado_string))
+print(type(dado_booleanum))
+
+# testando de um jeito diferente que eu não sabia
+
+print(dado_inteiro == dado_inteiro2)
+print(dado_inteiro == dado_inteiro3)
+
+print(dado_inteiro > dado_inteiro2)
+
+print(type(dado_inteiro == dado_inteiro2))
